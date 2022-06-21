8daa22c34c33bd60303e37495372f2b929f2d0b0
willian portela <willianportela1@gmail.com>
2022-06-21 14:47:03 -0300

    req 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+export default class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        {planetName}
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetImage: PropTypes.string.isRequired,
+  planetName: PropTypes.string.isRequired,
+};
