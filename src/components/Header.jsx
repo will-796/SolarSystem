1f51a5310405d33cc167bb060125631e48a06fa7
willian portela <willianportela1@gmail.com>
2022-06-21 15:10:16 -0300

    finalizando o projeto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <h1>Sistema Solar</h1>
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
     );
   }
 }
