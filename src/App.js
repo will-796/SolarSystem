a913d5f775a79ab101f15df20a9d801d45b579f8
willian portela <willianportela1@gmail.com>
2022-06-21 14:42:11 -0300

    req 3 e 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 // first commit
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
