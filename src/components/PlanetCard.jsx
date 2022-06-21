e91b8dce2d911b3ac0a69b8f8edf2d7c927795fd
willian portela <willianportela1@gmail.com>
2022-06-21 14:55:48 -0300

    req 6

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -6,7 +6,7 @@ export default class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        {planetName}
+        <p data-testid="planet-name">{planetName}</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 export default class SolarSystem extends Component {
   render() {
@@ -7,6 +9,9 @@ export default class SolarSystem extends Component {
       <div data-testid="solar-system">
         Sistema Solar
         <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (
+          <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+        ))}
       </div>
     );
   }
